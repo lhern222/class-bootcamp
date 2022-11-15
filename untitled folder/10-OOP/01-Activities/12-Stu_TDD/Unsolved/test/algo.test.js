1674c33b44abe5b83c55235e6755712e8bfdcd3e
lilia hern <lhern222@ucr.edu>
2022-11-14 19:23:20 -0800

    adding OOP file

--- untitled folder/10-OOP/01-Activities/12-Stu_TDD/Unsolved/test/algo.test.js
+++ untitled folder/10-OOP/01-Activities/12-Stu_TDD/Unsolved/test/algo.test.js
@@ -2,6 +2,14 @@ const Algo = require("../algo");
 
 describe("Algo", () => {
   describe("reverse", () => {
+    it("should reverse a given string", () => {
+      const str1 = "Hello World!";
+      const reverse = "?dlroW olleH";
+      const result = new Algo().reverse(str);
+     })
+    
+    const obj = new Algo ();
+    expect(str1 in obj).toEqual(reverse);
     // TODO: Write a test for the `reverse` method that should take a string as an argument and return a new reversed version of the string
   });
 
@@ -13,3 +21,6 @@ describe("Algo", () => {
     // TODO: Write a test for the `capitalize` method that should take a string as an argument and return a new string with the first letter of each word capitalized
   });
 });
+
+
+////screenshot takes 
--- untitled folder/10-OOP/01-Activities/14-Stu_Pass-Tests/Unsolved/algo.js
+++ untitled folder/10-OOP/01-Activities/14-Stu_Pass-Tests/Unsolved/algo.js
@@ -2,14 +2,26 @@ function Algo() {}
 
 Algo.prototype.reverse = function(str) {
   // TODO: Your code here
+  //for(let i = str.length-1; i > 0; i--){
+    //hello
+    //newString +=str[i];
+  //}                                     this can be a little more difficult 
+  return str.split('').reverse().join('');
+  // ^^this is an easier way in writing it 
 };
 
 Algo.prototype.isPalindrome = function(str) {
   // TODO: Your code here
+  return this.reverse(str) === str; //this will return true or false 
 };
 
 Algo.prototype.capitalize = function(str) {
   // TODO: Your code here 
+  //replace the first letter with an uppercase
+  ['this','is', 'a', 'test' ];
+  return str.split(' ').map((word) =>{
+    word.subtracting(0,1).toUpperCase();
+  } ).join(" ") //create the space on purpose, so you can have the word split 
 };
 
 module.exports = Algo;
